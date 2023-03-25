a3c91e9e42ce0c35da07c99d863db0cf9e19a421
Орлов Антон <fogelo@yandex.ru>
2023-03-25 13:14:29 +0700

    modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
+console.log(styles);
 
   return (
     <div className="App">
